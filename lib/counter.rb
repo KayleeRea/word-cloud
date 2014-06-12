fb02b375c3e2d7b5ad166fc2017e4afc07871afa
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-06-12 11:14:34 -0600

    Removed comments

--- lib/counter.rb
+++ lib/counter.rb
@@ -22,22 +22,3 @@ class Counter
     hash
   end
 end
-
-
-#
-#def words
-#  hash = {}
-#  @data.select do |name, words|
-#    words = words[1].split(' ')
-#    words.each do |word|
-#      if hash.has_key?(word)
-#        count = hash[word][:count] += 1
-#        people = hash[word][:people] << name
-#        hash.merge!(word => {count: count, people: people})
-#      else
-#        hash.merge!(word => {count: 1, people: [name]})
-#      end
-#    end
-#  end
-#  hash
-#end
